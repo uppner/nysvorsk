b7c253f1740cdd95f8721882f439111bfff82244
William Arvidsson <william.arvidsson@gmail.com>
2024-10-12 12:59:36 +0200

    Prettier

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -6,14 +6,12 @@ import vue from '@vitejs/plugin-vue'
 // https://vitejs.dev/config/
 export default defineConfig({
   build: {
-    outDir: 'build'    
+    outDir: 'build',
   },
-  plugins: [
-    vue(),
-  ],
+  plugins: [vue()],
   resolve: {
     alias: {
-      '@': fileURLToPath(new URL('./src', import.meta.url))
-    }
-  }
+      '@': fileURLToPath(new URL('./src', import.meta.url)),
+    },
+  },
 })
