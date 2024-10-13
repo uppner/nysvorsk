c4242832e1e8338393d94c83c06ffda4468df755
William Arvidsson <william.arvidsson@gmail.com>
2024-10-13 11:27:42 +0200

    POC fetch words from Cosmos DB

--- backend/Program.cs
+++ backend/Program.cs
@@ -1,13 +1,36 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Azure.Cosmos;
+
 var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+builder.Services.AddTransient<CosmosClient>(_ =>
+{
+  return new CosmosClient(builder.Configuration["CosmosConnectionString"]);
+});
 
 var app = builder.Build();
+
+
+var conn = app.Configuration["CosmosConnectionString"];
 
 app.UseSwagger();
 app.UseSwaggerUI();
 
+app.UsePathBase("/api");
+
 app.MapGet("/", () => "OK");
 
+app.MapGet("/words", async ([FromServices] CosmosClient client) =>
+{
+  var wordContainer = client.GetContainer("nysvorsk", "words");
+  var wordIterator = wordContainer.GetItemQueryIterator<Word>("select * from c");
+  var result = await wordIterator.ReadNextAsync();
+  var words = result.ToList();
+  return Results.Ok(words);
+});
+
 app.Run();
+
+public record Word(string name, string status, string text);
